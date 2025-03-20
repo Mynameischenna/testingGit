3629b95709cd82dd6576ea12446bc4aff4769014
Mynameischenna <89410670+Mynameischenna@users.noreply.github.com>
2025-03-20 16:57:07 +0530

    Update repo.java

--- repo.java
+++ repo.java
@@ -1,4 +1,18 @@
 
+
+PS C:\compliance1\ECM1> mvn clean package
+mvn : The term 'mvn' is not recognized as the name of a cmdlet, function, script file, or operable program. Check 
+the spelling of the name, or if a path was included, verify that the path is correct and try again.
+At line:1 char:1
++ mvn clean package
++ ~~~
+    + CategoryInfo          : ObjectNotFound: (mvn:String) [], CommandNotFoundException
+    + FullyQualifiedErrorId : CommandNotFoundException
+
+PS C:\compliance1\ECM1> 
+
+
+
 spackage com.example.firstt;
 
 import org.springframework.data.jpa.repository.JpaRepository;
